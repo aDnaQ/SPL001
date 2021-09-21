197fc74d0c5259ef691b35be196ac11838a6370b
aDnaQ <adna.music@bulme.at>
2021-09-21 12:52:38 +0200

    .gitignore geändert

---
+++ Blechberechnung/Blechberechnung/Blechberechnung.c
@@ -0,0 +1,10 @@
+// Blechberechnung.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello world");
+}
+
